7dfd026776e4af6009c411519f3a0e9aef611d34
Hosh <domingohoshaiah@gmail.com>
2023-07-24 22:08:35 +0800

    added login component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Home from './components/Home';
+import Login from './components/auth/Login';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Dashboard/>} />
         <Route exact path="/home" element={<Home/>} />
+        <Route exact path="/login" element={<Login/>} />
       </Routes>
     </Router>
   ); 
