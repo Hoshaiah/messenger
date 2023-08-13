9e2ee2c12c17af69c1aed7f7d15fa98edafb6ce5
Hosh <domingohoshaiah@gmail.com>
2023-08-14 06:44:26 +0800

    added logout feature

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,16 +1,26 @@
+import { useDispatch, useSelector } from "react-redux";
+import { logoutUser } from "../services/actionServices";
 import SearchBar from "./SearchBar";
-
-
-
-
-
-
+import { setAuthorization } from "../redux/currentuserSlice";
+import Cookies from "js-cookie";
 
 function TopNav () {
+    const currentuser = useSelector((state) => state.currentuser)
+    const dispatch = useDispatch()
+
+    const handleLogoutClick = async () => {
+        const logoutData = await logoutUser(currentuser.authorization)
+        if (logoutData.status === 200) {
+            dispatch(setAuthorization(''))
+            Cookies.remove('jwt_token');
+
+        }
+    }
 
     return (
         <div class="flex items-center justify-center w-full h-10 bg-slate-400" >
             <SearchBar/>
+            <button onClick={handleLogoutClick} >Logout</button>
         </div>
     )
 }
--- src/services/actionServices.js
+++ src/services/actionServices.js
@@ -1,6 +1,17 @@
 import Constants from "../constants"
 
 
+export const logoutUser = async (auth) => {
+    const resp = await fetch(`${Constants.server}logout`, {
+        method: 'DELETE', // Specify the request method as POST
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': auth,
+        },
+    })
+    const data = await resp.json()
+    return data
+}
 export const getUserChannels = async (auth) => {
     const resp = await fetch(`${Constants.server}channels`, {
         method: 'GET', // Specify the request method as POST
@@ -10,5 +21,4 @@ export const getUserChannels = async (auth) => {
         },
     })
     const data = await resp.json()
-    console.log(data)
 }
