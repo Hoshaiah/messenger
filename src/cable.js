dc3833b2f00113a5030997505e035d0fafd57644
Hosh <domingohoshaiah@gmail.com>
2023-09-14 09:39:56 +0800

    added friend request box

--- src/cable.js
+++ src/cable.js
@@ -1,5 +1,14 @@
 import { createConsumer } from '@rails/actioncable';
+import Constants from './constants';
+import Cookies from 'js-cookie';
+
 const URL = 'ws://127.0.0.1:3000/cable';
-const consumer = createConsumer(URL);
+// const URL = `ws://hosh-messenger-api.onrender.com/cable`;
+const consumer = createConsumer(URL, [], {
+    // cookie: `user.id=${Cookies.get('user_id')}`,
+    headers: {
+        // 'Origin': 'localhost:3001'
+    }
+});
  
 export default consumer;
--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { logoutUser } from "../services/actionServices";
 import SearchBar from "./SearchBar";
@@ -9,6 +10,9 @@ function TopNav () {
     const navigate = useNavigate()
     const currentuser = useSelector((state) => state.currentuser)
     const dispatch = useDispatch()
+    const [friendBox, setFriendBox] = useState()
+    const friendDiv = useRef()
+    const friendRequestButton = useRef()
 
     const handleLogoutClick = async () => {
         const logoutData = await logoutUser(currentuser.authorization)
@@ -28,10 +32,26 @@ function TopNav () {
         }
     }
 
+    useEffect(() => {
+        function handleClickOutside(event) {
+          if (friendDiv.current && !friendDiv.current.contains(event.target) && event.target!== friendRequestButton.current) {
+            setFriendBox(false)
+          }
+        }
+    
+        document.addEventListener('click', handleClickOutside);
+    
+        return () => {
+          document.removeEventListener('click', handleClickOutside);
+        };
+      }, []);
+
     return (
         <div class="flex items-center justify-center w-full h-10 bg-slate-800 border border-slate-600 overflow-visible" >
             <SearchBar/>
-            <button class= 'absolute right-0 mr-2 text-white' onClick={handleLogoutClick} >Logout</button>
+            <button ref={friendRequestButton} class= 'absolute right-28 mr-2 text-white' onClick={() => {setFriendBox(true)}} >Friend Requests</button>
+            <button class= 'absolute right-5 mr-2 text-white' onClick={handleLogoutClick} >Logout</button>
+            {friendBox && <div ref={friendDiv} class='absolute right-16 w-60 h-80 top-8 z-40 bg-slate-300'></div>}
         </div>
     )
 }
