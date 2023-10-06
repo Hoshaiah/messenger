6e9f782aa40f253d54ee20fbb498196a1de5d0ec
Hosh <domingohoshaiah@gmail.com>
2023-10-07 02:12:25 +0800

    changed render path

--- src/cable.js
+++ src/cable.js
@@ -2,8 +2,8 @@ import { createConsumer } from '@rails/actioncable';
 import Constants from './constants';
 import Cookies from 'js-cookie';
 
-const URL = 'ws://127.0.0.1:3000/cable';
-// const URL = `ws://hosh-messenger-api.onrender.com/cable`;
+// const URL = 'ws://127.0.0.1:3000/cable';
+const URL = `wss://hosh-messenger-api.onrender.com/cable`;
 const consumer = createConsumer(URL, [], {
     // cookie: `user.id=${Cookies.get('user_id')}`,
     headers: {
--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 
 const Constants = {
-    server: 'http://127.0.0.1:3000/'
-    // server: 'https://hosh-messenger-api.onrender.com/'
+    // server: 'http://127.0.0.1:3000/'
+    server: 'https://hosh-messenger-api.onrender.com/'
 }
 
 export default Constants;
